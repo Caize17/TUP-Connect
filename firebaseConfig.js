a4b6d856ff7a32c016cc5fc114838a071adf3c3b
nixxdc <delacruznicole0808@gmail.com>
2026-03-10 19:23:09 +0800

    Commit changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBpGOdMpx_Mws2EcCq6rbOWfZ-FFuhhfo0",
