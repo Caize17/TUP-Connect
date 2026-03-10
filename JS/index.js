dc4f19540a653f2dfb51e66b857498a51fd5626e
Camille <aizacamillealvarez@gmail.com>
2026-03-10 14:10:08 +0800

    sign in -> homepage

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,22 @@ function showError(message) {
     el.style.display = message ? 'block' : 'none';
   }
 }
+
+function handleHomepage() {
+  const email    = document.getElementById('login-email');
+  const password = document.getElementById('login-password');
+
+  let valid = true;
+
+  ['email', 'password'].forEach(f => {
+    const inputField = document.getElementById(`login-${f}`);
+    if (inputField) inputField.classList.remove('error');
+    
+    const errorLabel = document.getElementById(`err-${f}`);
+    if (errorLabel) errorLabel.classList.remove('visible');
+  });
+
+  if (valid) {
+    window.location.href = '../pages/homepage.html';
+  }
+}
